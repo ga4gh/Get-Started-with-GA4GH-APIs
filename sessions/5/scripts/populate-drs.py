02229d75882a50db8ad2084b9215d11ea1461332
Jeremy Adams <jeremy.adams@ga4gh.org>
2022-05-25 15:19:03 -0400

    script to add 5 recognized passport visas to DRS

---
+++ sessions/5/scripts/populate-drs.py
@@ -0,0 +1,10 @@
+def add_1k_genomes_lowcov_dataset_to_drs():
+    pass
+
+def main():
+    """Register all test dataset DRS Objects on local Starter Kit DRS server"""
+
+    add_1k_genomes_lowcov_dataset_to_drs()
+
+if __name__ == "__main__":
+    main()
